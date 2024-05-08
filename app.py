c6c40983e04a996feca5fac9989bb98dc75fbe21
Ralph's Code <RalphsCode@gmail.com>
2024-05-07 21:54:58 -0700

    Option to select a survey, answers saves to session.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash, session
+from flask import Flask, request, render_template, redirect, flash, session, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 import surveys
 
@@ -13,21 +13,32 @@ debug = DebugToolbarExtension(app)
 
 @app.route('/home')
 def home():
-	global survey 
-	survey = surveys.satisfaction_survey
 	return render_template('home.html', survey = survey)
 
 @app.route('/questions/<int:number>', methods=['GET', 'POST'])
 def questions(number):
 	global survey
+	# Check if there are any more questions
 	if number <= len(survey.questions):
+		# Check if its the fiirst question (which will not have any answer(s) yet.)
 		if number >=2:
-			print('setting session')
 			responses.append(request.form.get('answer', 'None'))
 			session['answers'] = responses
 		return render_template('questions.html', survey=survey, number=number)
+	
+	# If this was the last question:
 	else:
 		responses.append(request.form.get('answer', 'None'))
 		session['answers'] = responses
 		return render_template('thanks.html')
-  
+
+@app.route('/start')
+def start():  
+		global survey
+		choice = request.args.get('choice')
+		print('choice:', choice)
+		survey = getattr(surveys, choice)
+		print('survey:', survey)
+		return render_template('start.html', survey = survey)
+
+	
